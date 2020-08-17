0b3af3be837e02270d84f800a4d3bb4c4bdb0196
mwusdv <mwusdv@gmail.com>
2020-08-16 23:16:38 -0700

    400 episodes, max score 8

--- ddpg_agent.py
+++ ddpg_agent.py
@@ -10,11 +10,11 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 BUFFER_SIZE = int(1e5)  # replay buffer size
-BATCH_SIZE = 512        # minibatch size
+BATCH_SIZE = 128       # minibatch size
 GAMMA = 0.99            # discount factor
 TAU = 1e-3              # for soft update of target parameters
 LR_ACTOR = 1e-4         # learning rate of the actor 
-LR_CRITIC = 1e-3       # learning rate of the critic
+LR_CRITIC = 2e-4       # learning rate of the critic
 WEIGHT_DECAY = 0        # L2 weight decay
 UPDATE_EVERY = 20      # model is updated every UPDATE_EVERY time steps
 N_UPDATES = 10         # number of updates in every update
